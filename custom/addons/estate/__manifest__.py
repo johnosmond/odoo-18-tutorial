3ff8332603f2db692346d7795959b4d143b7a90a
John Osmond <john@jtech10.com>
2026-02-08 19:53:21 -0500

    ch02: scaffold estate addon

---
+++ custom/addons/estate/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'summary': 'Estate management',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
