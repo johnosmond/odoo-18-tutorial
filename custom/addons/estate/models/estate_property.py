b2b9af6b719fe98bd8d5b331ee2d6471562b7c07
John Osmond <john@jtech10.com>
2026-02-11 20:23:30 -0500

    ch05: add defaults and copy behavior to estate.property

--- custom/addons/estate/models/estate_property.py
+++ custom/addons/estate/models/estate_property.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -8,10 +9,13 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda self: fields.Date.today() + relativedelta(months=3)
+    )
+    expected_price = fields.Float(readonly=True, copy=False)
     selling_price = fields.Float(readonly=True)
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
