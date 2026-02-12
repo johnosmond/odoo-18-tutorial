f2aab4ac8ed5679987a996d736a86cf8562d3e49
John Osmond <john@jtech10.com>
2026-02-11 20:09:51 -0500

    ch05: moved menu commands to estate_menus.xml

--- custom/addons/estate/__manifest__.py
+++ custom/addons/estate/__manifest__.py
@@ -8,6 +8,7 @@
         'security/estate_security.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True,
