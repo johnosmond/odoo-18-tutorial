597433aaf16c7ac720540f2d0828da1a10ff262a
John Osmond <john@jtech10.com>
2026-02-12 21:09:58 -0500

    ch06: complete basic views exercises (form, list, search, archive)

--- custom/addons/estate/models/estate_property.py
+++ custom/addons/estate/models/estate_property.py
@@ -16,11 +16,11 @@ class EstateProperty(models.Model):
     expected_price = fields.Float(copy=False)
     selling_price = fields.Float(readonly=True)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string='Garden Area (sqm)')
     garden_orientation = fields.Selection(
         [
             ('north', 'North'),
